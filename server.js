cc4019c9e9d1a32fe395a9dee2793184bf5ed833
Lesli Ramos <les11@email.unc.edu>
2022-12-08 17:39:46 -0500

    added comments

--- server.js
+++ server.js
@@ -11,18 +11,22 @@ import Database from "better-sqlite3"
 const db = new Database('project.db');
 db.pragma('journal_mode = WAL');
 
+// create table for users which contain the username and password
 const sqlInit = `CREATE TABLE users ( user VARCHAR, pass VARCHAR);`;
 try {
   db.exec(sqlInit);
 } catch (error) {
 }
 
+// create table for data which contain the username, list of teachers, rating average, and average difficulty
 const sqlInit2 = `CREATE TABLE data ( user VARCHAR, teachers VARCHAR, rating VARCHAR, difficulty VARCHAR );`
 try {
     db.exec(sqlInit2);
 } catch (error) {
 }
 
+// create table for user interactions which contain the username, a message of what activity was performed,
+// and the time the interaction occurred
 const sqlInit3 = `CREATE TABLE logs ( user VARCHAR, message VARCHAR, time VARCHAR);`;
 try {
   db.exec(sqlInit3);
@@ -47,33 +51,33 @@ app.get('/app/', (req, res) => {
   const timeElapsed = Date.now();
   const today = new Date(timeElapsed);
   let user = req.app.get('user')
+
+  // Add interaction to logs database, given the username and time
   const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', ' a succesful login or return to home', '${today.toISOString()}');`;
   db.exec(stmt1)
   res.render('home');
 })
 
 app.post('/app/login', (req, res) => {
-    /*res.render('login-fail');*/
     const user = req.body.username;
     const pass = req.body.password;
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
 
+    // Add interaction to logs database, given the username and time
     const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'attempt to login', '${today.toISOString()}');`;
     db.exec(stmt1)
-
+    // Searching if username exists
     const stmt = db.prepare(`SELECT * FROM users WHERE user='${user}' and pass='${pass}';`);
     let row = stmt.get();
-    console.log(stmt.all());
+    // If username is undefined, then login-fail is rendered
     if (row === undefined) {
         req.app.set('user', user);
         req.app.set('pass', pass);
         res.render('login-fail')
-        //res.redirect('/newacc/');
     } else {
         req.app.set('user', user);
         req.app.set('pass', pass);
-        //res.redirect('/app/login/acc/');
         res.render('login-succ')
     }
     
@@ -84,12 +88,13 @@ app.post('/app/newacc', (req, res) => {
     const pass = req.body.password;
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
+    // Add interaction to logs database, given the username and time
     const stmt2 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'attempt to create new user account', '${today.toISOString()}');`;
     db.exec(stmt2)
-
+  
     const stmt1 = db.prepare(`SELECT * FROM users WHERE user='${user}'`);
     let row = stmt1.get();
-
+    // If username is undefined then create new user, else render create-fail
     if (row === undefined) {
         const stmt = `INSERT INTO users (user, pass) VALUES ('${user}', '${pass}');`;
         const stmt2 = `INSERT INTO data (user) VALUES ('${user}');`;
@@ -108,8 +113,10 @@ app.get('/app/login/history/', (req,res) => {
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     let user = req.app.get('user')
+    // Add interaction to logs database, given the username and time
     const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'viewed history', '${today.toISOString()}');`;
     db.exec(stmt1)
+    // Render all rows from logs database
     const row = db.prepare(`SELECT * FROM logs WHERE user = '${req.app.get('user')}';`);
     let all = row.all();
     res.render('history', {logs: all});
@@ -119,6 +126,7 @@ app.get('/app/login/acc/', (req,res) => {
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     let user = req.app.get('user')
+    // Add interaction to logs database, given the username and time
     const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'viewed account', '${today.toISOString()}');`;
     db.exec(stmt1)
     res.render('acc', {user: req.app.get('user'), pass: req.app.get('pass')});
@@ -133,8 +141,10 @@ app.post('/app/login/acc/update/', (req,res) => {
   const timeElapsed = Date.now();
   const today = new Date(timeElapsed);
   let user = req.app.get('user')
+  // Add interaction to logs database, given the username and time
   const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'updated account', '${today.toISOString()}');`;
   db.exec(stmt1)
+  // Update the users database to reflect new username and password
   const stm2 = `UPDATE users SET user = '${username}', pass = '${pass}' WHERE user = '${curruser}';`;
   db.exec(stm2)
   res.render('update', {user: req.app.get('user'), pass: req.app.get('pass')});
@@ -144,13 +154,16 @@ app.get('/app/login/delete', (req,res) =>{
   const timeElapsed = Date.now();
   const today = new Date(timeElapsed);
   let user1 = req.body.username
+  // Add interaction to logs database, given the username and time
   const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user1}', 'deleted account', '${today.toISOString()}');`;
   db.exec(stmt1)
 
   const user = req.body.username;
   const pass = req.body.password;
+  // Delete the users data from 'data' database
   const stmt2 = `DELETE FROM data WHERE user='${user}';`;
   db.exec(stmt2)
+  // Delete user from 'users' database
   const stmt = `DELETE FROM users WHERE user='${user}' and pass='${pass}';`;
   db.exec(stmt)
   
@@ -189,13 +202,17 @@ app.post('/app/login/ratings/', async(req, res) => {
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     let user = req.app.get('user')
+    // Add interaction to logs database, given the username and time
     const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'computed rating for user professors', '${today.toISOString()}');`;
     db.exec(stmt1)
+    // Add the rating information to the users row in the 'data' database
     const stmt2 = `INSERT INTO data (user, teachers, rating) VALUES ('${user}', '${teachersArr}', '${rating}');`;
     db.exec(stmt2)
     let difficulty = await computeDifficulty(teachersArr);
+    // Add interaction to logs database, given the username and time
     const stmt3 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'computed difficulty for user professors', '${today.toISOString()}');`;
     db.exec(stmt3)
+    // Add the difficulty information to the users row in the 'data' database
     const stmt4 = `INSERT INTO data (user, teachers, difficulty) VALUES ('${user}', '${teachersArr}', '${difficulty}');`;
     db.exec(stmt4)
     const truerating = rating.averageRating;
@@ -215,8 +232,10 @@ app.get('/app/login/ratings/:teachers/', async(req, res) => {
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     let user = req.app.get('user')
-    const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'computed rating for user proffessors', '${today.toISOString()}');`;
+    // Add interaction to logs database, given the username and time
+    const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'computed rating for user professors', '${today.toISOString()}');`;
     db.exec(stmt1)
+    // Add the rating information to the users row in the 'data' database
     const stmt2 = `INSERT INTO data (user, teachers, rating) VALUES ('${user}', '${teachersArr}', '${rating}');`;
     db.exec(stmt2)
     res.send(rating);
@@ -228,8 +247,10 @@ app.get('/app/login/difficulty/', async(req, res) => {
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     let user = req.app.get('user')
-    const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'computed difficulty of predetermined proffessors', '${today.toISOString()}');`;
+    // Add interaction to logs database, given the username and time
+    const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'computed difficulty of predetermined professors', '${today.toISOString()}');`;
     db.exec(stmt1)
+    // Add the difficulty information to the users row in the 'data' database
     const stmt2 = `INSERT INTO data (user, teachers, difficulty) VALUES ('${user}', '${teacherList}', '${difficulty}');`;
     db.exec(stmt2)
     res.send(difficulty);
@@ -247,8 +268,10 @@ app.get('/app/login/difficulty/:teachers/', async(req, res) => {
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     let user = req.app.get('user')
+    // Add interaction to logs database, given the username and time
     const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'computed difficulty for user professors', '${today.toISOString()}');`;
     db.exec(stmt1)
+    // Add the difficulty information to the users row in the 'data' database
     const stmt2 = `INSERT INTO data (user, teachers, difficulty) VALUES ('${user}', '${teachersArr}', '${difficulty}');`;
     db.exec(stmt2)
   
@@ -259,6 +282,7 @@ app.get("*",(req, res) => {
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     let user = req.app.get('user')
+    // Add interaction to logs database, given the username and time
     const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'entered invalid endpoint', '${today.toISOString()}');`;
     db.exec(stmt1)
 	res.status(404).send("404 NOT FOUND");
